63f2ddc45ce7b124a39c24dfdc8b7581fd19dc64
unknown <aja.washington13@gmail.com>
2020-04-02 15:47:35 -0500

    still broken, revert

--- BangazonAPI/Controllers/TrainingProgramsController.cs
+++ BangazonAPI/Controllers/TrainingProgramsController.cs
@@ -60,9 +60,13 @@ namespace BangazonAPI.Controllers
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                                        SELECT tp.Id, tp.Name, tp.MaxAttendees, tp.StartDate, tp.EndDate
-                                        FROM TrainingProgram tp 
-                                        WHERE tp.id LIKE @q";
+                    SELECT tp.Id, tp.Name, tp.StartDate, tp.MaxAttendees, tp.EndDate, 
+                                          e.Id, e.FirstName, e.LastName, e.DepartmentId, 
+                                              e.Email, e.IsSupervisor, e.ComputerId
+                                        FROM TrainingProgram tp
+                                        INNER JOIN EmployeeTraining et ON tp.Id = et.TrainingProgramId
+                                        INNER JOIN Employee e ON e.Id = et.EmployeeId
+                                        WHERE tp.Name LIKE @q";
                     cmd.Parameters.Add(new SqlParameter("@q", "%" + q + "%"));
                     SqlDataReader reader = cmd.ExecuteReader();
                     List<TrainingProgram> trainingPrograms = new List<TrainingProgram>();
@@ -73,62 +77,62 @@ namespace BangazonAPI.Controllers
                         var trainingProgramAlreadyAdded = trainingPrograms.FirstOrDefault(tprog => tprog.Id == trainingProgramId);
 
                         if (trainingProgramAlreadyAdded == null)
-                        { 
+                        {
 
                             TrainingProgram trainingProgram = new TrainingProgram
-                        {
-                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                            Name = reader.GetString(reader.GetOrdinal("Name")),
-                            StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
-                            EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
-                            MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees")),
-                            Employees = new List<Employee>()
-                          
-                        };
-
-                        trainingPrograms.Add(trainingProgram);
-                        
-                        var hasEmployees = !reader.IsDBNull(reader.GetOrdinal("Id"));
-                        if (hasEmployees)
-
-
-                            trainingProgram.Employees.Add(new Employee()
-                        {
-                                Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                                Email = reader.GetString(reader.GetOrdinal("Email")),
-                                IsSupervisor = reader.GetBoolean(reader.GetOrdinal("IsSupervisor"))
-                                //ComputerId = reader.GetInt32(reader.GetOrdinal("ComputerId")),
-                                //DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
-                                //computer = new Computer(),
-                                //department = new Department()
-                            }
-
-                        );
-
-
-                    }
-                    else
-
-                    {
-                        var hasEmployees = !reader.IsDBNull(reader.GetOrdinal("Id"));
-
-                        if (hasEmployees)
-                        {
-                            trainingProgramAlreadyAdded.Employees.Add(new Employee()
                             {
                                 Id = reader.GetInt32(reader.GetOrdinal("Id")),
-                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
-                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
-                                Email = reader.GetString(reader.GetOrdinal("Email")),
-                                IsSupervisor = reader.GetBoolean(reader.GetOrdinal("IsSupervisor"))
-                            });
-                        }
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate")),
+                                EndDate = reader.GetDateTime(reader.GetOrdinal("EndDate")),
+                                MaxAttendees = reader.GetInt32(reader.GetOrdinal("MaxAttendees")),
+                                Employees = new List<Employee>()
 
+                            };
+
+                            trainingPrograms.Add(trainingProgram);
+
+                            var hasEmployees = !reader.IsDBNull(reader.GetOrdinal("Id"));
+                            if (hasEmployees)
+
+
+                                trainingProgram.Employees.Add(new Employee()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                    Email = reader.GetString(reader.GetOrdinal("Email")),
+                                    IsSupervisor = reader.GetBoolean(reader.GetOrdinal("IsSupervisor"))
+                                    //ComputerId = reader.GetInt32(reader.GetOrdinal("ComputerId")),
+                                    //DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                    //computer = new Computer(),
+                                    //department = new Department()
+                                }
+
+                            );
+
+
+                        }
+                        else
+
+                        {
+                            var hasEmployees = !reader.IsDBNull(reader.GetOrdinal("Id"));
+
+                            if (hasEmployees)
+                            {
+                                trainingProgramAlreadyAdded.Employees.Add(new Employee()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                                    FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                    Email = reader.GetString(reader.GetOrdinal("Email")),
+                                    IsSupervisor = reader.GetBoolean(reader.GetOrdinal("IsSupervisor"))
+                                });
+                            }
+
+                        }
                     }
-                }
-                reader.Close();
+                    reader.Close();
 
                     return Ok(trainingPrograms);
 
@@ -186,100 +190,101 @@ namespace BangazonAPI.Controllers
                 }
             }
         }
+
+
+
+
+
+
+
+        //Add a trainingProgram          url: "api/trainingPrograms"         method: POST             result: TrainingProgram Object   
+        //[HttpPost]
+        //public async Task<IActionResult> Post([FromBody] TrainingProgram trainingProgram)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"INSERT INTO TrainingProgram (Name, Budget)
+        //                                OUTPUT INSERTED.Id
+        //                                VALUES (@Name, @Budget)";
+        //            cmd.Parameters.Add(new SqlParameter("@Name", trainingProgram.Name));
+        //            cmd.Parameters.Add(new SqlParameter("@Budget", trainingProgram.Budget));
+
+        //            int newId = (int)cmd.ExecuteScalar();
+        //            trainingProgram.Id = newId;
+        //            return CreatedAtRoute("GetTrainingProgram", new { id = newId }, trainingProgram);
+        //        }
+        //    }
+        //}
+
+        //        //Update a trainingProgram              url: "api/trainingPrograms/{id}"	           method: PUT     result: TrainingProgram Object    
+        //        [HttpPut("{id}")]
+        //        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] TrainingProgram trainingProgram)
+        //        {
+        //            try
+        //            {
+        //                using (SqlConnection conn = Connection)
+        //                {
+        //                    conn.Open();
+        //                    using (SqlCommand cmd = conn.CreateCommand())
+        //                    {
+        //                        cmd.CommandText = @"UPDATE TrainingProgram
+        //                                            SET Name = @Name,
+        //                                                Budget = @Budget
+        //                                            WHERE Id = @id";
+        //                        cmd.Parameters.Add(new SqlParameter("@Name", trainingProgram.Name));
+        //                        cmd.Parameters.Add(new SqlParameter("@Budget", trainingProgram.Budget));
+        //                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //                        int rowsAffected = cmd.ExecuteNonQuery();
+        //                        if (rowsAffected > 0)
+        //                        {
+        //                            return new StatusCodeResult(StatusCodes.Status204NoContent);
+        //                        }
+        //                        throw new Exception("No rows affected");
+        //                        throw new Exception("No rows affected");
+        //                    }
+        //                }
+        //            }
+        //            catch (Exception)
+        //            {
+        //                if (!TrainingProgramExists(id))
+        //                {
+        //                    return NotFound();
+        //                }
+        //                else
+        //                {
+        //                    throw;
+        //                }
+        //            }
+        //        }
+
+
+
+
+
+
+        //private bool TrainingProgramExists(int id)
+        //{
+        //    using (SqlConnection conn = Connection)
+        //    {
+        //        conn.Open();
+        //        using (SqlCommand cmd = conn.CreateCommand())
+        //        {
+        //            cmd.CommandText = @"
+        //                        SELECT Id, Name, Budget
+        //                        FROM TrainingProgram
+        //                        WHERE Id = @id";
+        //            cmd.Parameters.Add(new SqlParameter("@id", id));
+
+        //            SqlDataReader reader = cmd.ExecuteReader();
+        //            return reader.Read();
+        //        }
+        //    }
+        //        }
+        //    }
+        //}
     }
 }
-
-
-
-
-
-
-//        //Add a trainingProgram          url: "api/trainingPrograms"         method: POST             result: TrainingProgram Object   
-//        [HttpPost]
-//        public async Task<IActionResult> Post([FromBody] TrainingProgram trainingProgram)
-//        {
-//            using (SqlConnection conn = Connection)
-//            {
-//                conn.Open();
-//                using (SqlCommand cmd = conn.CreateCommand())
-//                {
-//                    cmd.CommandText = @"INSERT INTO TrainingProgram (Name, Budget)
-//                                        OUTPUT INSERTED.Id
-//                                        VALUES (@Name, @Budget)";
-//                    cmd.Parameters.Add(new SqlParameter("@Name", trainingProgram.Name));
-//                    cmd.Parameters.Add(new SqlParameter("@Budget", trainingProgram.Budget));
-
-//                    int newId = (int)cmd.ExecuteScalar();
-//                    trainingProgram.Id = newId;
-//                    return CreatedAtRoute("GetTrainingProgram", new { id = newId }, trainingProgram);
-//                }
-//            }
-//        }
-
-//        //Update a trainingProgram              url: "api/trainingPrograms/{id}"	           method: PUT     result: TrainingProgram Object    
-//        [HttpPut("{id}")]
-//        public async Task<IActionResult> Put([FromRoute] int id, [FromBody] TrainingProgram trainingProgram)
-//        {
-//            try
-//            {
-//                using (SqlConnection conn = Connection)
-//                {
-//                    conn.Open();
-//                    using (SqlCommand cmd = conn.CreateCommand())
-//                    {
-//                        cmd.CommandText = @"UPDATE TrainingProgram
-//                                            SET Name = @Name,
-//                                                Budget = @Budget
-//                                            WHERE Id = @id";
-//                        cmd.Parameters.Add(new SqlParameter("@Name", trainingProgram.Name));
-//                        cmd.Parameters.Add(new SqlParameter("@Budget", trainingProgram.Budget));
-//                        cmd.Parameters.Add(new SqlParameter("@id", id));
-
-//                        int rowsAffected = cmd.ExecuteNonQuery();
-//                        if (rowsAffected > 0)
-//                        {
-//                            return new StatusCodeResult(StatusCodes.Status204NoContent);
-//                        }
-//                        throw new Exception("No rows affected");
-//                        throw new Exception("No rows affected");
-//                    }
-//                }
-//            }
-//            catch (Exception)
-//            {
-//                if (!TrainingProgramExists(id))
-//                {
-//                    return NotFound();
-//                }
-//                else
-//                {
-//                    throw;
-//                }
-//            }
-//        }
-
-
-
-
-
-
-//private bool TrainingProgramExists(int id)
-//{
-//    using (SqlConnection conn = Connection)
-//    {
-//        conn.Open();
-//        using (SqlCommand cmd = conn.CreateCommand())
-//        {
-//            cmd.CommandText = @"
-//                        SELECT Id, Name, Budget
-//                        FROM TrainingProgram
-//                        WHERE Id = @id";
-//            cmd.Parameters.Add(new SqlParameter("@id", id));
-
-//            SqlDataReader reader = cmd.ExecuteReader();
-//            return reader.Read();
-//        }
-//    }
-//        }
-//    }
-//}
