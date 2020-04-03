9bb92a39995e8fe9e3aea7c03aea62be905a8ce0
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-04-03 10:21:28 -0500

    userpayment types is getting all and customerId param is working

---
+++ BangazonAPI/Controllers/UserPaymentTypesController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using BangazonAPI.Models;
+
+namespace BangazonAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserPaymentTypesController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        public UserPaymentTypesController(IConfiguration config)
+        {
+            _config = config;
+        }
+        public SqlConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        
+        //////----------GET----------
+        
+        [HttpGet]
+        public async Task<IActionResult> Get(
+               [FromQuery] string customerId
+               )
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        SELECT u.Id, u.AcctNumber, u.Active, u.CustomerId, u.PaymentTypeid 
+                        FROM UserPaymentType u
+                        WHERE u.Active = 1
+                        ";
+                    if (customerId != null)
+                    {
+                        cmd.CommandText += " AND u.Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@Id", customerId));
+                    };
+
+                  
+
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    List<UserPaymentType> allUserPaymentTypes = new List<UserPaymentType>();
+
+                    while (reader.Read())
+                    {
+                        var userPaymentType = new UserPaymentType()
+                        {
+
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            AcctNumber = reader.GetString(reader.GetOrdinal("AcctNumber")),
+                            Active = reader.GetBoolean(reader.GetOrdinal("Active")),
+                            CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                            PaymentTypeId = reader.GetInt32(reader.GetOrdinal("PaymentTypeId")),
+                     
+
+
+                        };
+
+                        allUserPaymentTypes.Add(userPaymentType);
+                    }
+                    reader.Close();
+
+                    return Ok(allUserPaymentTypes);
+                }
+            }
+        }
+    }
+}
