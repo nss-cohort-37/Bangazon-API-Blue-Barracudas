51d7350954438884644aa265cf32892e15a8ac4a
Mac Gibbons <57641175+macgibbons@users.noreply.github.com>
2020-04-03 11:00:33 -0500

    POST functional

--- BangazonAPI/Controllers/UserPaymentTypesController.cs
+++ BangazonAPI/Controllers/UserPaymentTypesController.cs
@@ -41,22 +41,26 @@ namespace BangazonAPI.Controllers
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
+                    // We dont want to return a full list of all the users payment types so we only want to get one at a type 
+                    //based off of their customerId so we have to add the customerId param
+
                     if (customerId != null)
                     {
+                        //because we only want active payment types we only get back where active = 1 (true)
                     cmd.CommandText = @"
                         SELECT u.Id, u.AcctNumber, u.Active, u.CustomerId, u.PaymentTypeid 
                         FROM UserPaymentType u
                         WHERE u.Active = 1
-                        AND u.Id = @id";
+                        AND u.CustomerId = @id";
+
                         cmd.Parameters.Add(new SqlParameter("@Id", customerId));
                     }
                     else
                     {
+                        //if you try to get all the users payment types you will get a forbidden error 
+
                         return new StatusCodeResult(StatusCodes.Status403Forbidden);
                     };
-
-                  
-
 
                     SqlDataReader reader = cmd.ExecuteReader();
 
@@ -72,9 +76,7 @@ namespace BangazonAPI.Controllers
                             Active = reader.GetBoolean(reader.GetOrdinal("Active")),
                             CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
                             PaymentTypeId = reader.GetInt32(reader.GetOrdinal("PaymentTypeId")),
-                     
-
-
+  
                         };
 
                         allUserPaymentTypes.Add(userPaymentType);
@@ -82,6 +84,35 @@ namespace BangazonAPI.Controllers
                     reader.Close();
 
                     return Ok(allUserPaymentTypes);
+                }
+            }
+        }
+
+        //----------POST----------
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] UserPaymentType userPaymentType)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                        INSERT INTO UserPaymentType (AcctNumber, Active, CustomerId, PaymentTypeId)
+                        OUTPUT INSERTED.Id
+                        VALUES (@AcctNumber, @Active, @CustomerId, @PaymentTypeId)";
+
+                    cmd.Parameters.Add(new SqlParameter("@AcctNumber", userPaymentType.AcctNumber));
+                    cmd.Parameters.Add(new SqlParameter("@Active", userPaymentType.Active));
+                    cmd.Parameters.Add(new SqlParameter("@CustomerId", userPaymentType.CustomerId));
+                    cmd.Parameters.Add(new SqlParameter("@PaymentTypeId", userPaymentType.PaymentTypeId));
+                    
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    userPaymentType.Id = id;
+                    return CreatedAtRoute("GetUserPaymentType", new { id = id }, userPaymentType);
                 }
             }
         }
